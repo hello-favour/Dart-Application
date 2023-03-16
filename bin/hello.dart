9d248ad319c803a210565e8c08e919d588f790f2
Favour <hello.williamsfav@gmail.com>
2023-03-16 21:53:21 +0100

    add more dart code

--- bin/hello.dart
+++ bin/hello.dart
@@ -1,4 +1,12 @@
 import 'dart:math';
+import 'package:test/expect.dart';
+
+enum Weather {
+  sunny,
+  snowy,
+  cloudy,
+  rainy,
+}
 
 void main() {
   print(22 ~/ 7);
@@ -91,11 +99,13 @@ void main() {
   call += ' Wenderlich';
   print(call);
 
+//string interpolation
   const number = 10;
   const multiplier = 5;
   final summary = "$number $multiplier = ${number + multiplier}";
   print(summary);
 
+//if else operator
   const isOneEqualTwo = (1 != 2);
   print(isOneEqualTwo);
 
@@ -129,6 +139,7 @@ void main() {
   const dogEqualsCat = guess == 'cat';
   print(dogEqualsCat);
 
+//if else condition
   if (2 > 1) {
     print('Yes, 2 is greater than 1.');
   }
@@ -139,4 +150,142 @@ void main() {
   } else {
     print('Animal is not a house pet.');
   }
+
+  const trafficLight = 'yellow';
+  var command = '';
+  if (trafficLight == 'red') {
+    command = 'Stop';
+  } else if (trafficLight == 'yellow') {
+    command = 'Slow down';
+  } else if (trafficLight == 'green') {
+    command = 'Go';
+  } else {
+    command = 'INVALID COLOR!';
+  }
+  print(command);
+
+//ternary operator
+  const scoreExam = 83;
+  final String Student = (scoreExam >= 60) ? 'You passed' : 'You failed';
+  print(Student);
+
+  const int count = 50;
+  final countMe = (count != null) ? "it's not null" : "it's null";
+  print(countMe);
+  const hisAge = 22;
+  final isMyAge = (hisAge == 19) ? "Teenager" : "His not teenager";
+  print(isMyAge);
+
+  //switch case statement
+  const numberMe = 3;
+  switch (numberMe) {
+    case 0:
+      print("zero");
+      break;
+    case 1:
+      print("one");
+      break;
+    case 2:
+      print("two");
+      break;
+    case 3:
+      print("three");
+      break;
+    default:
+      print("none");
+  }
+
+  const weatherToday = Weather.rainy;
+  switch (weatherToday) {
+    case Weather.cloudy:
+      print("It was clouddy");
+      break;
+    case Weather.rainy:
+      print("rain was falling heavly here");
+      break;
+    case Weather.sunny:
+      print("it was sunny");
+      break;
+    case Weather.snowy:
+      print("snowing today");
+      break;
+  }
+// if (number == 0) {
+// print('zero');
+// } else if (number == 1) {
+// print('one');
+// } else if (number == 2) {
+// print('two');
+// } else if (number == 3) {
+// print('three');
+// } else if (number == 4) {
+// print('four');
+// } else {
+// print('something else');
+// }
+//loop
+  const numberList = [2, 3, 4, 5];
+  numberList.forEach((element) {
+    print(element);
+  });
+  const numbers = [1, 2, 4, 7, 8];
+  numbers.forEach((e) => print(e));
+
+  for (var i = 0; i < 10; i++) {
+    print(i);
+  }
+
+  for (var i = 0; i < 5; i++) {
+    if (i == 2) {
+      continue;
+    }
+    print(i);
+  }
+
+  var sum = 0;
+  for (var i = 0; i <= 5; i++) {
+    if (i == 2) {
+      continue;
+    }
+    print(sum += i);
+  }
+  for (var i = 10; i > 4; i--) {
+    if (i == 3) {
+      break;
+    }
+    print(i);
+  }
+
+//Function
+  const inPut = 30;
+  String outPut = compliment(inPut);
+  print(outPut);
+
+  final name1 = fullNamee("dada", "dem");
+  print(name1);
+
+  print(withinTolerance(3, 2, 5));
+  final mutiplyNumber = multiply(12, 3);
+  print(mutiplyNumber);
+  // print(multiply(2, 3));
 }
+
+String compliment(int number) {
+  return "$number is very nice number";
+}
+
+String fullNamee(String choose, String lastMe, [String? main]) {
+  if (main != null) {
+    return '$main $choose $lastMe';
+  } else {
+    return '$choose $lastMe';
+  }
+}
+
+bool withinTolerance(int value, [int min = 0, int max = 10]) {
+  return min <= value && value <= max;
+}
+
+Function multiply = (int a, int b) {
+  return a * b;
+};
